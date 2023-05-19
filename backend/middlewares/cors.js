e1ea981416a153f7dbfcd4c40947849a15d33042
Maria Zelenova <zma611@yandex.ru>
2023-05-19 17:26:21 +0300

    change cors header

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -10,7 +10,8 @@ const allowedCors = [
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
+  const requestHeaders = req.headers['Content-Type'];
+  // const requestHeaders = req.headers['access-control-request-headers'];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   res.header('Access-Control-Allow-Credentials', true);
 
